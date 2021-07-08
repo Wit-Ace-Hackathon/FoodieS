7d422b2e2917a9460fdb83ee725f1f6f6408e946
Aman Kulshreshtha <52315847+Aman-kulshreshtha@users.noreply.github.com>
2021-07-08 17:11:54 +0530

    Update _DBConnect.php
    remote sql connection

--- Website/scripts/_DBConnect.php
+++ Website/scripts/_DBConnect.php
@@ -1,9 +1,17 @@
 <?php
-$servername = "localhost:3307";
-$username = "root";
-$password = "" ;
-$database = "foodwebsite";
+//development server 
+//$servername = "localhost:3307";
+//$username = "root";
+//$password = "" ;
+//$database = "foodwebsite";
 
+
+//production server
+
+$servername = "remotemysql.com";
+$username = "6tuOj3AD92";
+$password = "Itgspbw6uY" ;
+$database = "6tuOj3AD92";
 $conn = mysqli_connect($servername,$username,$password,$database);
 //if($conn) {
 //    echo "Connected";
