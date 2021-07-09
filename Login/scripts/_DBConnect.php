474a5f72c502d3e91859b631f6e7e9477eeecebd
himanshit0304 <70479061+himanshit0304@users.noreply.github.com>
2021-07-09 16:16:56 +0530

    Update _DBConnect.php

--- Login/scripts/_DBConnect.php
+++ Login/scripts/_DBConnect.php
@@ -1,9 +1,5 @@
 <?php
-//development server 
-//$servername = "localhost:3307";
-//$username = "root";
-//$password = "" ;
-//$database = "foodwebsite";
+
 
 
 //production server
@@ -13,7 +9,5 @@ $username = "6tuOj3AD92";
 $password = "Itgspbw6uY" ;
 $database = "6tuOj3AD92";
 $conn = mysqli_connect($servername,$username,$password,$database);
-//if($conn) {
-//    echo "Connected";
-//}
+
 ?>
